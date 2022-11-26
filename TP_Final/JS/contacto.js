f2104083c2eebc03a6c8ed42a23da634114910b8
EricSchenone <ericschenone90@gmail.com>
2022-11-26 15:30:19 -0300

    trabajo-final

---
+++ TP_Final/JS/contacto.js
@@ -0,0 +1,20 @@
+"use stricct";
+var formElement = document.getElementById("form");
+var inputs = document.querySelectorAll("input");
+var consulta = document.getElementById("mensaje");
+var alerta = document.getElementById("alerta");
+formElement.addEventListener("submit", capturarDatos);
+function capturarDatos(e) {
+    e.preventDefault();
+    for (var i = 0; i < inputs.length - 1; i++) {
+        if (inputs[i].value == "" || consulta.value == "") {
+            alerta.innerHTML = "Debe completar todos los campos. *";
+        }
+        else {
+            console.log(inputs[i].value);
+            alerta.innerHTML = "";
+        }
+    }
+    console.log(consulta.value);
+    formElement.reset();
+}
